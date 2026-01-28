a69f3d8e7eb0ded62693264befe14914af7c9584
Daniel Gaviria Arboleda <daniel.gaviria.utp@gmail.com>
2026-01-28 00:26:30 -0500

    Update board config and pin mappings for MM100
    Expanded and updated the custom board configuration in board_configuration.cpp to assign specific MM100 pins for injection, ignition, sensor channels, and other outputs. Updated custom_firmware.yaml to adjust pin names and add new output pins for MM100_INJ5 and MM100_INJ6, reflecting the new hardware mappings.

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -8,26 +8,62 @@ static void customBoardDefaultConfiguration() {
     setHellenVbatt();
     setHellenCan();
     setDefaultHellenAtPullUps();
-    // engineConfiguration->injectionPins[0] = Gpio::F13;
-    // engineConfiguration->ignitionPins[0] = Gpio::E15;
+    hellenMegaSdWithAccelerometer();
+    setHellenMegaEnPin();
+    setHellenMMbaro();
 
-//   engineConfiguration->triggerInputPins[0] = Gpio::B1;
-//	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
+    engineConfiguration->injectionPins[0] = Gpio::MM100_INJ1;
+	engineConfiguration->injectionPins[1] = Gpio::MM100_INJ2;
+	engineConfiguration->injectionPins[2] = Gpio::MM100_INJ3;
+    engineConfiguration->injectionPins[3] = Gpio::MM100_INJ4;
 
-//	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
+    engineConfiguration->ignitionPins[0] = Gpio::MM100_IGN1;
+	engineConfiguration->ignitionPins[1] = Gpio::MM100_IGN2;
+	engineConfiguration->ignitionPins[2] = Gpio::MM100_IGN3;
 
-//	engineConfiguration->clt.adcChannel = EFI_ADC_1;
+    engineConfiguration->tps1_1AdcChannel = MM100_IN_TPS_ANALOG;
+	engineConfiguration->map.sensor.hwChannel = MM100_IN_MAP1_ANALOG;
+	engineConfiguration->clt.adcChannel = MM100_IN_CLT_ANALOG;
+	engineConfiguration->iat.adcChannel = MM100_IN_IAT_ANALOG;
 
-//	engineConfiguration->iat.adcChannel = EFI_ADC_2;
+	engineConfiguration->triggerInputPins[0] = Gpio::MM100_UART8_TX; // VR2 max9924 is the safer default
+	engineConfiguration->camInputs[0] = Gpio::MM100_IN_D1;
 
+    engineConfiguration->displayLogicLevelsInEngineSniffer = true;
+	engineConfiguration->globalTriggerAngleOffset = 0;
+	engineConfiguration->canTxPin = Gpio::MM100_CAN_TX;
+	engineConfiguration->canRxPin = Gpio::MM100_CAN_RX;
+	engineConfiguration->fanPin = Gpio::MM100_OUT_PWM1;
+	engineConfiguration->fuelPumpPin = Gpio::MM100_OUT_PWM2;
+	engineConfiguration->tachOutputPin = Gpio::MM100_INJ5;
+    
+    // PWM pin
+	engineConfiguration->stepperDcIo[0].controlPin = Gpio::MM100_OUT_PWM3;
+	// DIR pin
+	engineConfiguration->stepperDcIo[0].directionPin1 = Gpio::MM100_OUT_PWM4;
+	// Disable pin
+	engineConfiguration->stepperDcIo[0].disablePin = Gpio::MM100_SPI2_MISO;
+	// PWM pin
+	engineConfiguration->stepperDcIo[1].controlPin = Gpio::MM100_OUT_PWM5;
+	// DIR pin
+	engineConfiguration->stepperDcIo[1].directionPin1 = Gpio::MM100_SPI2_MOSI;
+	// Disable pin
+	engineConfiguration->stepperDcIo[1].disablePin = Gpio::MM100_USB1ID;
 
-    	// 5.6k high side/10k low side = 1.56 ratio divider
-  //  	engineConfiguration->analogInputDividerCoefficient = 1.56f;
+    setupDefaultSensorInputs();
 
-    	// 6.34k high side/ 1k low side
-//    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
+    engineConfiguration->enableVerboseCanTx = true;
+	engineConfiguration->injectionMode = IM_BATCH;
+	engineConfiguration->etbFunctions[0] = DC_None;
 
-//	engineConfiguration->adcVcc = 3.3f;
+    setCrankOperationMode();
+    setAlgorithm(LM_SPEED_DENSITY);
+
+    setCommonNTCSensor(&engineConfiguration->clt, HELLEN_DEFAULT_AT_PULLUP);
+	setCommonNTCSensor(&engineConfiguration->iat, HELLEN_DEFAULT_AT_PULLUP);
+
+	setTPS1Calibration(100, 650);
+
 }
 
 void setup_custom_board_overrides() {
