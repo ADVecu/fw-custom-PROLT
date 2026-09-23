2b1ad23bed3a5bf383d5df450c2e7c98165a2600
GitHub git update Action <action@github.com>
2026-09-23 19:51:19 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_PRO_LT.h
+++ generated/controllers/generated/signature_PRO_LT.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 4203973548
-#define TS_SIGNATURE "rusEFI main.2026.09.22.PRO_LT.4203973548"
+#define SIGNATURE_HASH 174101836
+#define TS_SIGNATURE "rusEFI main.2026.09.23.PRO_LT.174101836"
--- generated/controllers/lua/generated/output_lookup_generated.cpp
+++ generated/controllers/lua/generated/output_lookup_generated.cpp
@@ -934,6 +934,9 @@ float getOutputValueByHash(const int hash) {
 // transitionEventsCounter
 		case 1163075685:
 			return engine->outputChannels.transitionEventsCounter;
+// slowAdcScanCount
+		case -900042688:
+			return engine->outputChannels.slowAdcScanCount;
 // cltResistance
 		case -1105741991:
 			return engine->outputChannels.cltResistance;
@@ -952,6 +955,9 @@ float getOutputValueByHash(const int hash) {
 // engine
 		case -75965445:
 			return engine->outputChannels.engine;
+// sdCardMode
+		case 545972859:
+			return engine->outputChannels.sdCardMode;
 // baseDwell
 		case -1777838088:
 			return engine->ignitionState.baseDwell;
